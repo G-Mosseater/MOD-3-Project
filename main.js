6297042c4c0786cbe45ef2f6091743c35cc15c88
Gabriel <Gabriel.bachelor@gmail.com>
2024-10-09 10:42:19 +0000

    changed nav bar

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 "use strict";
 {
-    let message = 'Hello, HELLO!';
-    console.log(message);
+    const icon = document.getElementById("icon");
+    icon.addEventListener('click', function () {
+        icon.classList.toggle('active_icon');
+    });
 }
