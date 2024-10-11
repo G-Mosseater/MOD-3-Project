c25dabc5fdee3380716238736d775abbb99575de
audurinez <audurinse@gmail.com>
2024-10-11 16:01:07 +0000

    buttons activated

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@
 {
 const icon = document.getElementById("icon") 
 
-icon.addEventListener('click', function(){
-    icon.classList.toggle('active_icon')
+icon?.addEventListener('click', function(){
+    icon?.classList.toggle('active_icon')
 })
+ }
