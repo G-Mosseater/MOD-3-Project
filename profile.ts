a9ef9d5f75297399fe8e7ce17cf2a6810ce51aaa
audurinez <audurinse@gmail.com>
2024-10-13 21:05:00 +0000

    js button fixes

--- profile.ts
+++ profile.ts
@@ -1,9 +1,18 @@
 console.log("test")
+
 const buttons:NodeListOf<HTMLDivElement>=document.querySelectorAll(".button")
+
 console.log("buttons",buttons)
+
 buttons.forEach(button=>{
     button.addEventListener("click", event=>{
-        button.style.backgroundColor="#FFECC5"
+
+
+        if (button.style.backgroundColor==="rgb(255, 236, 197)" || button.style.backgroundColor === "#ffecc5"){
+            button.style.backgroundColor="#FFFFFF"
+        } else {
+            button.style.backgroundColor="#FFECC5"
+        }
 
     })
 })
