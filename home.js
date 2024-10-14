8066f5bd579d4ac0284c48d3c400e9cc7a5a1bd0
ln0185 <helene.delaunayratel@gmail.com>
2024-10-14 16:13:27 +0000

    fixes

--- home.js
+++ home.js
@@ -1,13 +1,32 @@
 "use strict";
-const news = document.getElementById("favorites-container");
+const newscontainer = document.getElementById("favorites-container");
 const events = JSON.parse(localStorage.getItem("events") || "");
-events.forEach((event) => {
-    const container = document.createElement("div");
-    container.innerHTML = event;
-    const star = container.querySelector(".star") || document.createElement("img");
-    const alt = star.alt;
-    const singlenews = container.querySelector(".news");
-    if (alt === "favorited item" && singlenews) {
-        news === null || news === void 0 ? void 0 : news.appendChild(singlenews);
-    }
-});
+const unfavorite = (star) => {
+    star.src = "./img/star icon.svg";
+    star.alt = "click to favorite";
+    showFavorite();
+};
+const post = (singlenews) => {
+    newscontainer === null || newscontainer === void 0 ? void 0 : newscontainer.appendChild(singlenews);
+};
+const showFavorite = () => {
+    if (!newscontainer)
+        return 0;
+    newscontainer.innerHTML = "";
+    events.forEach((event, i) => {
+        const container = document.createElement("div");
+        container.innerHTML = event;
+        const singlenews = container.querySelector(".news");
+        const star = container.querySelector(".star") || document.createElement("img");
+        if (star.alt === "favorited item" && singlenews) {
+            post(singlenews);
+        }
+        star.addEventListener("click", () => {
+            var _a, _b;
+            unfavorite(star);
+            events[i] = (_b = (_a = star.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.outerHTML;
+        });
+    });
+    localStorage.setItem("events", JSON.stringify(events));
+};
+showFavorite();
