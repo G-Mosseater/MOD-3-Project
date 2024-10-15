712ec4241085090ff29ffa350562c050fbd9a451
Gabriel <Gabriel.bachelor@gmail.com>
2024-10-15 14:14:08 +0000

    javascript

--- mom_chat.ts
+++ mom_chat.ts
@@ -13,7 +13,7 @@ async function getJoke() {
     },
   });
   const jokeObj = await jokeData.json();
-  console.log(jokeObj);
+  return jokeObj;
 }
 
 if (chatForm && chatInput) {
@@ -29,5 +29,33 @@ if (chatForm && chatInput) {
     chatContainer.appendChild(chatDiv);
     chatInput.value = "";
     chatContainer.scrollTop = chatContainer.scrollHeight;
+    const time = new Date();
+    chatDiv.innerHTML += `<div class="time_left"><p>${getPerfectTime()}</p></div>
+`;
   });
 }
+
+const postJoke = async () => {
+  if (!chatInput) return 0;
+  const chatDiv = document.createElement("div");
+  chatDiv.classList.add("chat_left");
+  const jokeObject = await getJoke();
+  const chatText = jokeObject.joke;
+  chatDiv.innerHTML = `<p>${chatText}</p>`;
+  if (!chatContainer) return 0;
+  chatContainer.appendChild(chatDiv);
+  chatInput.value = "";
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+};
+postJoke();
+
+setInterval(postJoke, 15000);
+
+const getPerfectTime = (time: Date = new Date()) => {
+  const minutes = (time.getMinutes() < 10 ? "0" : "") + time.getMinutes();
+  const seconds = (time.getSeconds() < 10 ? "0" : "") + time.getSeconds();
+  const hours = (time.getHours() < 10 ? "0" : "") + time.getHours();
+  return hours + ":" + minutes +  ":" + seconds
+};
+
+console.log(getPerfectTime());
